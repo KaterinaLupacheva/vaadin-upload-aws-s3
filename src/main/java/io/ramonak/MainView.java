b54c4fd0cf0d505472aaff0a54e3c9755cc1f27b
Katerina <katerinalupacheva@gmail.com>
2020-03-14 19:07:10 +0300

    link and image components added

--- src/main/java/io/ramonak/MainView.java
+++ src/main/java/io/ramonak/MainView.java
@@ -1,20 +1,41 @@
 package io.ramonak;
 
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.io.ByteArrayInputStream;
 
 @Route
 @CssImport("./styles/shared-styles.css")
 public class MainView extends VerticalLayout {
+
+    private final Image image;
+
+    private final UploadS3 upload;
 
     public MainView(@Value("${aws.accessKey}") String accessKey,
                     @Value("${aws.secretKey}") String secretKey,
                     @Value("${aws.s3bucket.name}") String bucketName) {
         addClassName("centered-content");
 
-        UploadS3 upload = new UploadS3(accessKey, secretKey, bucketName);
-        add(upload);
+        TextField link = new TextField("Link");
+        link.setWidthFull();
+
+        upload = new UploadS3(accessKey, secretKey, bucketName);
+        upload.uploadFile(link);
+        image = new Image("", "image");
+        link.addValueChangeListener(e -> {
+            byte[] imageBytes = upload.downloadImage();
+            StreamResource resource = new StreamResource("image",
+                    () -> new ByteArrayInputStream(imageBytes));
+            image.setSrc(resource);
+            add(image);
+        });
+        add(upload, link);
     }
 }
